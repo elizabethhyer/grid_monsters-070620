fd11fed6bf0f19a68d635c28c9b07a128747a8b2
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-28 17:24:43 -0400

    Makes changes to #monsterfy_names_forward and #monsterfy_names_backward methods and adds new method, #change_location

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,4 +1,4 @@
-require_relative "../environment.rb"
+require_relative "../config/environment.rb"
 
 class CommandLineInterface
 
@@ -10,39 +10,8 @@ class CommandLineInterface
         puts "Let the games begin"
     end 
 
-    def monsterfy_names_forward
-        user_name = ""
-        monster_names_forward = ["#{user_name}, Child of the Machines", "#{user_name}, Invader from the Nether Realms", "#{user_name}, Human Upgrade v.2.0", "#{user_name}...do you know that you are a Replicant?", "#{user_name}, Star Hunter"]
-        puts "Please tell us your name."
-        user_name = gets.chomp
-        new_name = monster_names_forward.sample
-    end 
-
-
-    def monsterfy_names_backward
-        user_name = ""
-        monster_names_backward = ["Count #{user_name}, Bloodthirsty Lord of the Night", "#{user_name}, Child of the Grave", "#{user_name}, Disciple of the Wolf and Moon", "#{user_name}, Wyvern of the Blue Dragonflight", "#{user_name}, Caller of Cthulu", "#{user_name}, Leader of the Deathly Coven", "#{user_name}, Butcher of the Bay"]
-        user_name = gets.chomp
-        puts "Please tell us your name."
-        user_name = gets.chomp
-        new_name = monster_names_backward.sample
-    end 
-
-    def new_location_welcome(new_location, new_name)
-        puts "Welcome to #{new_location}, #{new_name}..."
-        sleep 2
-        puts "You have entered a realm of Monsters and Demons and most importantly..."
-        sleep 3
-        puts "writers."
-    end 
-
-    def call
-        user_input = ""
-        until user_input == "exit"
-        greeting    
-        sleep 5
+    def change_location
         puts "Please tell our scientists whether you would like to go forwards or backwards in time."
-        puts "To quit, type 'exit'."
         user_input = gets.chomp
         if user_input == "forwards" 
             
@@ -51,6 +20,38 @@ class CommandLineInterface
         elsif user_input == "backwards"
           
         elsif user_input == "backward"
+    end 
+
+    def monsterfy_names_forward
+        puts "Please tell us your name."
+        user_name = gets.chomp
+        monster_names_forward = ["#{user_name}, Child of the Machines", "#{user_name}, Invader from the Nether Realms", "#{user_name}, Human Upgrade v.2.0", "#{user_name}...do you know that you are a Replicant?", "#{user_name}, Star Hunter"]
+        new_name = monster_names_forward.sample
+    end 
+
+
+    def monsterfy_names_backward
+        puts "Please tell us your name."
+        user_name = gets.chomp
+        monster_names_backward = ["Count #{user_name}, Bloodthirsty Lord of the Night", "#{user_name}, Child of the Grave", "#{user_name}, Disciple of the Wolf and Moon", "#{user_name}, Wyvern of the Blue Dragonflight", "#{user_name}, Caller of Cthulu", "#{user_name}, Leader of the Deathly Coven", "#{user_name}, Butcher of the Bay"]
+        new_name = monster_names_backward.sample
+    end 
+
+    def new_location_welcome
+        if 
+        puts "Welcome to #{new_location}, #{name}..."
+        sleep 2
+        puts "You have entered a realm of Monsters and Demons and most importantly..."
+        sleep 3
+        puts "writers."
+    end 
+
+    def call
+        greeting    
+        sleep 5
+        user_input = ""
+        until user_input == "exit"
+        puts "To quit, type 'exit'."
             
         # else raise InputError
         end 
